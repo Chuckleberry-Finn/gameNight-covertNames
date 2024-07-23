c6e105e7cb8513fb3ac98316fff01c1847cae10e
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-07-22 21:42:18 -0400

    tweaks

--- Contents/mods/main/media/lua/client/gameNight - implementation CovertNames.lua
+++ Contents/mods/main/media/lua/client/gameNight - implementation CovertNames.lua
@@ -4,18 +4,29 @@ local gamePieceAndBoardHandler = applyItemDetails.gamePieceAndBoardHandler
 
 
 local words = {}
-for i=1, 400 do table.insert(words, "word ("..i..")") end
-deckActionHandler.addDeck("CovertNamesWords", words)
+local wordsAltNames = {}
+for i=1, 400 do
+    local wordID = "word ("..i..")"
+    table.insert(words, wordID)
+    wordsAltNames[wordID] = "Covert Name Tile"
+end
+
+deckActionHandler.addDeck("CovertNamesWords", words, wordsAltNames)
 gamePieceAndBoardHandler.registerSpecial("Base.CovertNamesWords", {
     actions = { examine=true },
     examineScale = 1,
-    textureSize = {180,100}
+    textureSize = {124,69}
 })
 
 
 local keyCards = {}
-for i=1, 40 do table.insert(keyCards, "Keycard"..i) end
-deckActionHandler.addDeck("CovertNamesKeyCards", keyCards)
+local keyCardsAltNames = {}
+for i=1, 40 do
+    local KeyCardID = "Keycard"..i
+    table.insert(keyCards, KeyCardID)
+    keyCardsAltNames[KeyCardID] = "Key Card"
+end
+deckActionHandler.addDeck("CovertNamesKeyCards", keyCards, keyCardsAltNames)
 gamePieceAndBoardHandler.registerSpecial("Base.CovertNamesKeyCards", {
     actions = { examine=true },
     examineScale = 1,
@@ -23,20 +34,26 @@ gamePieceAndBoardHandler.registerSpecial("Base.CovertNamesKeyCards", {
 })
 
 
-local agents = {"Assassin","AgentRedDouble","AgentBlueDouble"}
-for i=1, 7 do table.insert(agents, "Civilian") end
-local colors = {"red","blue"}
+local agents = {"Assassin"}
+local agentsAltNames = {}
 
+for i=1, 7 do table.insert(agents, "Civilian") end
+local colors = {"Red","Blue"}
 for _,color in pairs(colors) do
     for i=1, 4 do
-        table.insert(agents, "Agent"..color..i)
-        table.insert(agents, "Agent"..color..i)
+        local agentID = "Agent"..color..i
+        table.insert(agents, agentID)
+        table.insert(agents, agentID)
+        agentsAltNames[agentID] = color.." Agent"
     end
+    local doubleAgentID = "Agent"..color.."Double"
+    table.insert(agents, doubleAgentID)
+    agentsAltNames[doubleAgentID] = color.." Double Agent"
 end
 
-deckActionHandler.addDeck("CovertNamesAgents", agents)
+deckActionHandler.addDeck("CovertNamesAgents", agents, agentsAltNames)
 gamePieceAndBoardHandler.registerSpecial("Base.CovertNamesAgents", {
     actions = { examine=true },
     examineScale = 1,
-    textureSize = {180,100}
+    textureSize = {125,69}
 })
